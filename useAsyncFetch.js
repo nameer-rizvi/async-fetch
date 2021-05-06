dc2eca000c993be9c2a81d74ffad1ba0b1515ac5
Nameer Rizvi <rizvinameer@gmail.com>
2021-05-05 21:58:40 -0400

    refactoring

--- useAsyncFetch.js
+++ useAsyncFetch.js
@@ -34,77 +34,80 @@ function useAsyncFetch(props, fetchProps) {
   const [data, setData] = useState(initialData);
   const [unmounted, setUnmounted] = useState();
 
-  const cancelActiveRequest = () =>
-    controller && controller.abort && controller.abort();
-
-  const handle = {
-    start: (props = {}) => {
-      disableController !== true && cancelActiveRequest();
-      !props.excludePendingUpdate && setPending(true);
-      setError();
-      onStart && onStart();
-    },
-    initialResponse: (initialResponse) => {
-      if (!initialResponse.ok) {
-        throw Error(
-          initialResponse.statusText || initialResponse.status.toString()
-        );
-      }
-      return initialResponse[initialResponseParser]();
-    },
-    success: (parsedResponse) => {
-      setData(parsedResponse);
-      onSuccess && onSuccess(parsedResponse);
-    },
-    fail: (err) => {
-      setError(err);
-      onFail && onFail(err);
-    },
-    finish: () => {
-      setPending();
-      onFinish && onFinish();
-    },
-  };
-
-  function sendRequest(props = {}) {
-    !unmounted && handle.start(props);
-    query = query ? "?" + new URLSearchParams(query).toString() : "";
-    controller = new AbortController();
-    const options = {
-      method,
-      signal: controller && controller.signal,
-      ...fetchOptions,
-      body:
-        (bodyData || fetchOptions.body) &&
-        JSON.stringify({ ...bodyData, ...fetchOptions.body }),
-      ...fetchProps,
-    };
-    !unmounted &&
-      fetch(url + query, options)
-        .then((r) => !unmounted && handle.initialResponse(r))
-        .then((r) => !unmounted && handle.success(r))
-        .catch((e) => !unmounted && handle.fail(e))
-        .finally(() => !unmounted && handle.finish());
+  function cancelActiveRequest() {
+    if (controller && controller.abort) controller.abort();
   }
 
-  // Removed !pending condition for allowing retries on stalled requests.
-  const isValidRequest = url && !manual && condition !== false;
+  function sendRequest(props = {}) {
+    if (url && condition !== false && !unmounted) {
+      if (disableController !== true) cancelActiveRequest();
+
+      if (!props.excludePendingUpdate) setPending(true);
+
+      setError();
+
+      if (onStart) onStart();
+
+      query = query ? "?" + new URLSearchParams(query).toString() : "";
+
+      controller = new AbortController();
+
+      const options = {
+        method,
+        signal: controller && controller.signal,
+        ...fetchOptions,
+        body:
+          (bodyData || fetchOptions.body) &&
+          JSON.stringify({ ...bodyData, ...fetchOptions.body }),
+        ...fetchProps,
+      };
+
+      fetch(url + query, options)
+        .then((initialResponse) => {
+          if (!unmounted) {
+            if (!initialResponse.ok) {
+              const throwError =
+                initialResponse.statusText || initialResponse.status.toString();
+              throw Error(throwError);
+            }
+            return initialResponse[initialResponseParser]();
+          }
+        })
+        .then((parsedResponse) => {
+          if (!unmounted) {
+            setData(parsedResponse);
+            if (onSuccess) onSuccess(parsedResponse);
+          }
+        })
+        .catch((responseError) => {
+          if (!unmounted) {
+            setError(responseError);
+            if (onFail) onFail(responseError);
+          }
+        })
+        .finally(() => {
+          if (!unmounted) {
+            setPending();
+            if (onFinish) onFinish();
+          }
+        });
+    }
+  }
 
   useEffect(() => {
-    !url && console.warn("[async-fetch] url is required.");
-    isValidRequest && sendRequest();
+    if (!url) console.warn("[async-fetch] url is required.");
+    if (!manual) sendRequest();
     // eslint-disable-next-line
   }, useEffectDependency);
 
-  useInterval(
-    () => isValidRequest && sendRequest({ excludePendingUpdate: true }),
-    poll
-  );
+  useInterval(() => {
+    sendRequest({ excludePendingUpdate: true });
+  }, poll);
 
   useEffect(
     () => () => {
       setUnmounted(true);
-      disableController !== true && cancelActiveRequest();
+      if (disableController !== true) cancelActiveRequest();
     },
     // eslint-disable-next-line
     []
